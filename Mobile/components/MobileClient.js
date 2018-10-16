89dc9c899e48d78f52fd24a9a480c1b5e3a29fad
Angelina Panteley <angelina.panteley@triboo.it>
2018-10-16 17:45:48 +0300

    add base layout for mobile app

---
+++ Mobile/components/MobileClient.js
@@ -0,0 +1,73 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './MobileClient.css';
+
+class MobileClient extends React.PureComponent {
+
+  static propTypes = {
+    client: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      fam: PropTypes.string.isRequired,
+      im: PropTypes.string.isRequired,
+      otch: PropTypes.string.isRequired,
+      balance: PropTypes.number.isRequired
+    }),
+  };
+
+  state = {
+    FIO: {
+      fam: this.props.client.fam,
+      im: this.props.client.im,
+      otch: this.props.client.otch,
+    },
+    balance: this.props.client.balance,
+    isActive: this.props.client.balance >= 0,
+  };
+
+  componentWillReceiveProps = (newProps) => {
+    console.log("MobileClient id="+newProps.client.id+" componentWillReceiveProps");
+
+    this.setState({ FIO:{
+      fam: newProps.client.fam,
+      im: newProps.client.im,
+      otch: newProps.client.otch,
+    }, balance: newProps.client.balance });
+  };
+
+  render() {
+
+    console.log("MobileClient id="+this.props.client.id+" render");
+    const FIO = this.state.FIO;
+    
+    return (
+      <div className='MobileClient'>
+        <div className='MobileClientFIO'>
+          <span>{FIO.fam+" "+FIO.im+" "+FIO.otch}</span>
+        </div>
+        <div className='MobileClientBalance'>
+          <button>-</button>
+          <span>{this.state.balance}</span>
+          <button>+</button>
+        </div>
+        <div className='MobileClientStatus'>
+          {
+            this.state.isActive
+            ?
+            'Активен'
+            :
+            'Заблокирован'
+          }
+        </div>
+        <div className='MobileClientActions'>
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
+      </div>
+    );
+
+  }
+
+}
+
+export default MobileClient;
---
+++ Mobile/components/MobileCompany.js
@@ -0,0 +1,100 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import MobileClient from './MobileClient';
+
+import './MobileCompany.css';
+
+class MobileCompany extends React.PureComponent {
+
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    clients:PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        fam: PropTypes.string.isRequired,
+        im: PropTypes.string.isRequired,
+        otch: PropTypes.string.isRequired,
+        balance: PropTypes.number.isRequired,
+      })
+    ),
+  };
+
+  state = {
+    name: this.props.name,
+    clients: this.props.clients,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.clientsForRender = this.state.clients.map( client => <MobileClient key={client.id} client={client} /> );
+  };
+
+  setName = (e) => {
+    this.setState({name:e.target.value});
+  };
+  
+  setClientBalance = (clientId,newBalance) => {
+    let changed=false;
+    let newClients=[...this.state.clients];
+    newClients.forEach( (c,i) => {
+      if ( c.id==clientId ) {
+        let newClient={...c};
+        newClient.balance=newBalance;
+        newClients[i]=newClient;
+        changed=true;
+      }
+    } );
+    if ( changed )
+      this.setState({clients:newClients}, this.clientsUpdate);
+  };
+
+  setClientFIO = (clientId,newFIO) => {
+    let changed=false;
+    let newClients=[...this.state.clients];
+    newClients.forEach( (c,i) => {
+      if ( c.id==clientId && (c.fam!==FIO.fam || c.im!==FIO.im || c.otch!==FIO.otch )) {
+        let newClient={...c};
+        newClient.fam=FIO.fam;
+        newClient.im=FIO.im;
+        newClient.otch=FIO.otch;
+        newClients[i]=newClient;
+        changed=true;
+      }
+    } );
+    if ( changed ) 
+      this.setState({clients:newClients}, this.clientsUpdate);
+  };
+
+  clientsUpdate(newProps, newState) {
+    this.clientsForRender=newState.clients.map( client => {
+      let FIO={fam:client.fam,im:client.im,otch:client.otch};
+      return <MobileClient key={client.id} id={client.id} FIO={FIO} balance={client.balance} />;
+    });
+  }
+  
+  render() {
+
+    console.log("MobileCompany render");
+
+    return (
+      <div className='MobileCompany'>
+        <div className="MobileCompanyChangeTitle">
+          <input type="button" value="МТС" onClick={this.setName} />
+          <input type="button" value="Velcom" onClick={this.setName} />
+        </div> 
+        <div className='MobileCompanyName'>Компания &laquo;{this.state.name}&raquo;</div>
+        <div className='MobileCompanyClients'>
+          { this.clientsForRender }
+        </div>
+        <button className="MobileNewClientButton">Добавить</button>
+      </div>
+    )
+    ;
+
+  }
+
+}
+
+export default MobileCompany;

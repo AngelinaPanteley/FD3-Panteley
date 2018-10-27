0d0af849324890a0c9962b526a6af047d6b204de
Angelina Panteley <angelina.panteley@triboo.it>
2018-10-27 10:27:23 +0300

    add new tests for mobile company

--- Mobile/__test__/MobileCompany.test.js
+++ Mobile/__test__/MobileCompany.test.js
@@ -19,7 +19,7 @@ describe('>>>MobileCompany', () => {
   let clientsArr = [
     { id: 101, fam: "Иванов", im: "Иван", otch: "Иванович", balance: 2 },
     { id: 105, fam: "Сидоров", im: "Сидор", otch: "Сидорович", balance: 3 },
-    { id: 110, fam: "Петров", im: "Пётр", otch: "Петрович", balance: 1 },
+    { id: 110, fam: "Петров", im: "Пётр", otch: "Петрович", balance: -1 },
     { id: 120, fam: "Григорьев", im: "Григорий", otch: "Григорьевич", balance: 5 },
   ];
 
@@ -56,9 +56,49 @@ describe('>>>MobileCompany', () => {
     chaiExpect(wrapper.contains(<div className='MobileCompanyName'>Компания &laquo;{companyName}&raquo;</div>)).to.equal(true);
   });
 
-  it('renders company`s name', () => {
+  it('deletes client correctly', () => {
     wrapper.instance().deleteClient(101);
+    expect(wrapper.state('clients').length).toEqual(3);
     expect(wrapper.state('clients')[0].id).toEqual(105);
+  });
+
+  it('sets client`s balance correctly', () => {
+    wrapper.instance().setClientBalance(101, -1);
+    expect(wrapper.state('clients')[0].balance).toEqual(-1);
+  });
+
+  it('sets client`s FIO correctly', () => {
+    wrapper.instance().setClientFIO(101, {
+      fam: 'new fam',
+      im: 'new im',
+      otch: 'new otch',
+    });
+    expect(wrapper.state('clients')[0].fam).toEqual('new fam');
+    expect(wrapper.state('clients')[0].im).toEqual('new im');
+    expect(wrapper.state('clients')[0].otch).toEqual('new otch');
+  });
+
+  it('adds new client correctly', () => {
+    wrapper.instance().addClient();
+    const clients = wrapper.state('clients');
+    expect(clients.length).toEqual(5);
+    expect(clients[clients.length-1].id).toEqual(121);
+    expect(clients[clients.length-1].isEdit).toEqual(true);
+  });
+
+  it('filter clients correctly', () => {
+    const event = {
+      target: {
+        value: 'active',
+      }
+    }
+
+    wrapper.instance().filterClients(event);
+    expect(wrapper.state('filteredClients').length).toEqual(3);
+    
+    event.target.value = 'blocked';
+    wrapper.instance().filterClients(event);
+    expect(wrapper.state('filteredClients').length).toEqual(1);
   });
 
 });
